8d39366138bfe5f0138381855214c63977afbe26
William Morgan <wmorgan2023@gmail.com>
2026-03-05 11:14:57 -0500

    Added card class

---
+++ lib/models/card.dart
@@ -0,0 +1,50 @@
+class PlayingCard {
+  final int? id;
+  final String cardName;
+  final String suit;
+  final String? imageUrl;
+  final int folderId;
+
+  PlayingCard({
+    this.id,
+    required this.cardName,
+    required this.suit,
+    this.imageUrl,
+    required this.folderId,
+  });
+
+  Map<String, dynamic> toMap() => {
+    'id': id,
+    'card_name': cardName,
+    'suit': suit,
+    'image_url': imageUrl,
+    'folder_id': folderId,
+  };
+
+  factory PlayingCard.fromMap(Map<String, dynamic> map) => PlayingCard(
+    id: map['id'],
+    cardName: map['card_name'],
+    suit: map['suit'],
+    imageUrl: map['image_url'],
+    folderId: map['folder_id'],
+  );
+
+  PlayingCard copyWith({
+    int? id,
+    String? cardName,
+    String? suit,
+    String? imageUrl,
+    int? folderId,
+  }) =>
+      PlayingCard(
+        id: id ?? this.id,
+        cardName: cardName ?? this.cardName,
+        suit: suit ?? this.suit,
+        imageUrl: imageUrl ?? this.imageUrl,
+        folderId: folderId ?? this.folderId,
+      );
+
+  @override
+  String toString() =>
+      'PlayingCard{id: $id, cardName: $cardName, suit: $suit, folderId: $folderId}';
+}
